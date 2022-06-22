054c3ee3f098327bacc2ca665de6eef6639ab1a7
JagdishACIWRM <106968070+JagdishACIWRM@users.noreply.github.com>
2022-06-22 22:56:14 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,3 +11,12 @@ headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
 " > ~/.streamlit/config.toml
+
+echo "\
+[theme]\n\
+primaryColor = """#E694FF"""\n\
+backgroundColor = """#00172B"""\n\
+secondaryBackgroundColor = """#008388"""\n\
+textColor = """#FFF"""\n\
+font = """sans serif"""\n\
+"  >> ~/.streamlit/config.toml
